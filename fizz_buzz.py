1e2a204ff67ad215c96570d5052e21e570fcfe5e
bmdundar24 <86871573+bmdundar24@users.noreply.github.com>
2021-08-11 12:27:55 +0300

    created Fizz_Buzz

---
+++ fizz_buzz.py
@@ -0,0 +1,42 @@
+fizz = []
+def fizzbuzz():
+    list_total = []
+    list_rest = []
+    for i in range(1,101):
+        if (i % 3 == 0) and (i % 5 == 0):
+            list_total.append(i)
+            list_total[i-1] = "FizzBuzz"
+        elif i % 3 == 0:
+            list_total.append(i)
+            list_total[i-1] = "Fizz"
+            
+        elif i % 5 == 0:
+            list_total.append(i)
+            list_total[i-1] = "Buzz"
+        else:
+            list_total.append(i)
+    for j in range(1,101):
+        if (j % 3 == 0) or (j % 5 == 0):
+            pass
+        else:
+            list_rest.append(j)
+    
+        
+    print(list_total) # if you want one line ı u will use return
+    print(list_rest)
+print(fizzbuzz())
+
+    
+    
+    
+    
+    
+    
+"""
+    fizz = list(filter(lambda x : x % 3 == 0 , list_total))
+    for j in fizz:
+        if j in list_total:
+            a = str(list_total).replace(str(j),"fizz")
+        return a
+print(fizzbuzz())
+"""
