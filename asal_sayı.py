2640692478c3aae2f3385de34ab3a4bb67316ed7
bmdundar24 <86871573+bmdundar24@users.noreply.github.com>
2021-09-14 20:55:29 +0300

    conflig solve

--- asal_sayı.py
+++ asal_sayı.py
@@ -5,6 +5,6 @@ for i in range(2,n-1):
     if n % i == 0:
         list_1.append(i)
 if list_1:
-    print(f"{n} sayısı asal sayı değildir")
+    print(f"{n} sayısı asal sayı değildir.")
 else:
-    print(f"{n} sayısı asal sayıdır")
+    print(f"{n} sayısı asal sayıdır.")
