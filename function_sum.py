964c380e5d9f47a114c407072cfe8c15460c3e18
bmdundar24 <86871573+bmdundar24@users.noreply.github.com>
2021-09-14 20:56:55 +0300

    added

--- function_sum.py
+++ function_sum.py
@@ -1,7 +1,7 @@
 def summ_between(x,y):
     summ = 0
     x = int(input("Please enter first number : "))
-    y = int(input("Please enter second number : "))
+    y = int(input("Please enter second nmber : "))
     for i in range(x+1,y):
         summ = summ + i 
     return summ
