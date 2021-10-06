f3d52b65ff22bc9961469ea1d14f59d075c0764d
Burak Mustafa Dündar <86871573+bmdundar24@users.noreply.github.com>
2021-10-06 15:59:29 +0300

    Update chinese_puzzle.py

--- chinese_puzzle.py
+++ chinese_puzzle.py
@@ -4,4 +4,4 @@ a, b = 4, 2
 for i in range(1,94):
     for j in range(1,94):
         if ((a * i) + (b * j)) == 94:
-            print(f"{i} adet rabbit ve {j} adet tavuk vardır")
+            print(f"{i} adet rabbit ve {j} adet tavuk vardır.")
